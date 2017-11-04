41f8269180451898cbc79335ce1e44028a8dcdab
HalilCan <halilcanmemoglu@gmail.com>
2017-11-03 21:59:20 -0400

    Initial commit

---
+++ rock.js
@@ -0,0 +1,25 @@
+/**
+ * Created by hcm on 03.11.2017.
+ */
+let http = require('http');
+let fs = require('fs');
+let request = require('request');
+
+let send = function(imei, username, password, data) {
+  request.post(
+    'https://core.rock7.com/rockblock',
+    { json: {
+      imei: imei,
+      username: username,
+      password: password,
+      data: data,
+    } },
+    function (error, response, body) {
+      if (!error && response.statusCode === 200) {
+        console.log(body);
+      }
+    }
+  );
+}
+
+module.exports.request = send;
