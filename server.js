caa09d423d942f3e920abff37fa0800a3670c115
HalilCan <halilcanmemoglu@gmail.com>
2017-11-19 13:00:05 -0500

    Switching to express.js

--- server.js
+++ server.js
@@ -10,6 +10,15 @@ var bodyParser = require('body-parser');
 var rock = require('./rock');
 var request = require('request');
 var url = require('url');
+var fs = require('fs');
+
+var express = require('express');
+var app = express();
+
+//To parse x-www-form-urlencoded request bodies, Express.js can use urlencoded()
+//middleware from the body-parser module.
+var bodyparser = require('body-parser');
+app.use(bodyparser.urlencoded({extended: false}));
 
 var index = require('./routes/index');
 var users = require('./routes/users');
@@ -17,6 +26,7 @@ var users = require('./routes/users');
 // Main listening port for RockBlock Server communication
 var listeningPort = 8000;
 
+// This is how we send messages to the RockBlock servers
 var send = function(imei, username, password, data) {
   request.post(
     'https://core.rock7.com/rockblock',
@@ -44,22 +54,32 @@ var logData = function(url_obj) {
   var iridiumLongitude = url_obj.query.iridium_longitude;
   var iridiumCep = url_obj.query.iridium_cep; //estimate of the accuracy of lat-long in km
   var data = url_obj.query.data;
+  
+  //TODO: log data to the server database
   console.log(imei + '\n' + data);
 };
 
+//TODO: setup the rocblock server router to the /incoming url
+app.post('/incoming', function(req, res){
+  // Use URL to parse the request and get a URL object from it.
+  var urlObject = url.parse(req.url, true);
+  
+  // Get the path from the url
+  var urlPath = urlObject.pathname;
+  
+  // Now we will log the data
+  logData(urlObject);
+  
+  // RockBlock documentation requires us to respond with http status 200
+  res.writeHead(200, {'Content-Type': 'application/json'});
+  return res;
+});
+
+
+app.listen(listeningPort);
+
 var server = http.createServer(function (req, res) {
   if (req.method === 'POST') {
-    // Use URL to parse the request and get a URL object from it.
-    var urlObject = url.parse(req.url, true);
   
-    // Get the path from the url
-    var urlPath = urlObject.pathname;
-    
-    // Now we will log the data
-    logData(urlObject);
-    
-    // RockBlock documentation requires us to respond with http status 200
-    res.writeHead(200, {'Content-Type': 'application/json'});
-    return res;
   }
 }).listen(listeningPort);
