d7170244b9f8cf365a3a58116beb43d2f5fd04fe
HalilCan <halilcanmemoglu@gmail.com>
2017-11-20 23:55:04 -0500

    Clientside js

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -4,14 +4,37 @@
 
 // You REALLY want async = true.
 // Otherwise, it'll block ALL execution waiting for server response.
-let async = true;
+var async = true;
 
 var rock = require('../../rock');
 var hexify = require('../../hexify');
 
-let imeiBox = document.getElementById("imei-box");
-let usernameBox = document.getElementById("username-box");
-let passwordBox = document.getElementById("password-box");
+var imeiBox = document.getElementById("imei-box");
+var usernameBox = document.getElementById("username-box");
+var passwordBox = document.getElementById("password-box");
 
-let sendMessageButton = document.getElementById("send-message-button");
-let sendMessageBox = document.getElementById("send-message-box");
+var messageButton = document.getElementById("send-message-button");
+var messageBox = document.getElementById("msg-box");
+
+
+var sendMessage = $(function() {
+  $('message-form').submit(function(event) {
+    //We don't want the form to redirect the client
+    event.preventDefault();
+    //Record transceiver configuration data
+    var IMEI = imeiBox.value;
+    var USERNAME = usernameBox.value;
+    var PASSWORD = passwordBox.value;
+    var message = messageBox.value;
+    var hexMessage = hexify.encode(message);
+    var messageObject = {
+      imei: IMEI,
+      username: USERNAME,
+      password: PASSWORD,
+      data: hexMessage
+    };
+    $.post('/send-message', data, function(res) {
+      console.log(res);
+    })
+  });
+});
