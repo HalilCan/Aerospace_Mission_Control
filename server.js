072bb5f461196f9d1acad0749603003a11bdc659
HalilCan <halilcanmemoglu@gmail.com>
2017-11-24 22:11:11 -0500

    Pass JSON object instead of string to logData

--- server.js
+++ server.js
@@ -94,10 +94,11 @@ app.post('/incoming', function(req, res){
   //The data sent from RB is in req.body
   var formData = req.body;
   console.log(formData);
-  console.log(JSON.parse(formData));
+  console.log(formData.imei);
+  //console.log(JSON.parse(formData));
   
   // We will log the data
-  logData(JSON.parse(formData));
+  //logData(JSON.parse(formData));
   
   // RockBlock documentation requires us to respond with http status 200
   res.writeHead(200, {'Content-Type': 'application/json'});
