5c4decca46579eaac88b8d9a5350b7d6aa3ecbcc
HalilCan <halilcanmemoglu@gmail.com>
2017-12-02 22:43:49 -0500

    Map implementation

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -98,7 +98,7 @@ function sendToServer() {
 }
 
 //Google Maps part
-function initMap(latLngObjArray) {
+/*function initMap(latLngObjArray) {
     var map = new google.maps.Map(document.getElementById('map'), {
         zoom: 3,
         center: {lat: 0, lng: -180},
@@ -120,6 +120,11 @@ function initMap(latLngObjArray) {
 }
 
 initMap([{lat: 37.101, lng: -122.214},{lat: 47.101, lng: -125.214}]);
+*/
+
+function initMap() { var map = new google.maps.Map(document.getElementById('map'), { zoom: 3, center: {lat: 0, lng: -180}, mapTypeId: 'terrain' });
+    var flightPlanCoordinates = [ {lat: 37.772, lng: -122.214}, {lat: 21.291, lng: -157.821}, {lat: -18.142, lng: 178.431}, {lat: -27.467, lng: 153.027} ];
+    var flightPath = new google.maps.Polyline({ path: flightPlanCoordinates, geodesic: true, strokeColor: '#FF0000', strokeOpacity: 1.0, strokeWeight: 2 }); flightPath.setMap(map); }
 
 /* $('message-form').submit(function(event) {
   //We don't want the form to redirect the client
