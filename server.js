bf7b126ff0ff82aae160160a80685487d5f283c7
HalilCan <halilcanmemoglu@gmail.com>
2018-02-02 16:15:19 -0500

    formatting

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ var express = require('express');
 var app = express();
 
 //Port config for Heroku, 5000 for Webstorm
-app.set( 'port', ( process.env.PORT || 8000 ));
+app.set('port', ( process.env.PORT || 8000 ));
 
 var server = app.listen(app.get('port') || 8000);
 var io = require('socket.io')(server);
@@ -49,41 +49,50 @@ class incomingMessage {
     this.data = data;
     this.dehexData = hexify.decode(data);
   }
+  
   getImei() {
-        return this.imei;
+    return this.imei;
   }
+  
   getMomsn() {
-      return this.momsn;
+    return this.momsn;
   }
+  
   getTransmitTime() {
-      return this.transmitTime;
+    return this.transmitTime;
   }
+  
   getIridiumLatitude() {
-      return this.iridiumLatitude;
+    return this.iridiumLatitude;
   }
+  
   getIridiumLongitude() {
-      return this.iridiumLongitude;
+    return this.iridiumLongitude;
   }
+  
   getIridiumAccuracy() {
-      return this.iridiumCep;
+    return this.iridiumCep;
   }
+  
   getData() {
-      return this.data;
+    return this.data;
   }
+  
   getDecodedData() {
-      return this.dehexData;
+    return this.dehexData;
   }
+  
   getObject() {
-      return {
-        'imei' : this.imei,
-        'momsn' : this.momsn,
-        'transmit_time' : this.transmitTime,
-        'iridium_latitude' : this.iridiumLatitude,
-        'iridium_longitude' : this.iridiumLongitude,
-        'accuracy' : this.iridiumCep,
-        'hex_data' : this.data,
-        'data' : this.dehexData
-      };
+    return {
+      'imei': this.imei,
+      'momsn': this.momsn,
+      'transmit_time': this.transmitTime,
+      'iridium_latitude': this.iridiumLatitude,
+      'iridium_longitude': this.iridiumLongitude,
+      'accuracy': this.iridiumCep,
+      'hex_data': this.data,
+      'data': this.dehexData
+    };
   }
 }
 
@@ -95,7 +104,7 @@ var latitude = 0.0;
 var longitude = 0.0;
 var gpsAccuracy = 0.0;
 
-var logData = function(imei, momsn, transmitTime, irLat, irLon, irCep, data) {
+var logData = function (imei, momsn, transmitTime, irLat, irLon, irCep, data) {
   // Collect all the important information from the message;
   var imei = imei;
   var momsn = momsn;
@@ -104,63 +113,82 @@ var logData = function(imei, momsn, transmitTime, irLat, irLon, irCep, data) {
   var iridiumLongitude = irLon;
   var iridiumCep = irCep; //estimate of the accuracy of lat-long in km
   var data = data;
-
+  
   var newMessage = new incomingMessage(imei, momsn, transmitTime, iridiumLatitude, iridiumLongitude, iridiumCep, data);
   inbox.push(newMessage);
   console.log(inbox[0].getDecodedData());
-
+  
   latitude = iridiumLatitude;
   longitude = iridiumLongitude;
   gpsAccuracy = iridiumCep;
-
-  io.sockets.emit('new_message', {'timestamp' : transmitTime, 'data': hexify.decode(data), 'latitude':latitude, 'longitude':longitude, 'acc':gpsAccuracy});
-
+  
+  io.sockets.emit('new_message', {
+    'timestamp': transmitTime,
+    'data': hexify.decode(data),
+    'latitude': latitude,
+    'longitude': longitude,
+    'acc': gpsAccuracy
+  });
+  
   //TODO: log data to the server database
   console.log('\n' + imei + '\n' + data);
   console.log('data: ' + hexify.decode(data));
 };
 
 /* inputs:
-    gasType: Helium or Hydrogen, launchVolume in cubic meters, balloonWeight and payloadWeight in grams
-  outputs:
-    burstHeight in ft, ascentRate in ft/min, neutralLift in kg, burstTime in minutes
-*/
-function calculateBurst (gasType, launchVolume, balloonWeight, payloadWeight) {
-  var totexDataDict = {'200':[3.00,0.25], '300':[3.78,0.25], '350':[4.12,0.25], '450':[4.72,0.25],
-      '500':[4.99,0.25], '600':[6.02,0.3], '700':[6.53,0.3], '800':[7.00,0.3], '1000':[7.86,0.3],
-      '1200':[8.63,0.25], '1500':[9.44,0.25], '2000':[10.54,0.25], '3000':[13.00,0.25]};
-
+ gasType: Helium or Hydrogen, launchVolume in cubic meters, balloonWeight and payloadWeight in grams
+ outputs:
+ burstHeight in ft, ascentRate in ft/min, neutralLift in kg, burstTime in minutes
+ */
+function calculateBurst(gasType, launchVolume, balloonWeight, payloadWeight) {
+  var totexDataDict = {
+    '200': [3.00, 0.25],
+    '300': [3.78, 0.25],
+    '350': [4.12, 0.25],
+    '450': [4.72, 0.25],
+    '500': [4.99, 0.25],
+    '600': [6.02, 0.3],
+    '700': [6.53, 0.3],
+    '800': [7.00, 0.3],
+    '1000': [7.86, 0.3],
+    '1200': [8.63, 0.25],
+    '1500': [9.44, 0.25],
+    '2000': [10.54, 0.25],
+    '3000': [13.00, 0.25]
+  };
+  
   const heliumDensity = 0.1786; //at 0C, 101kPa
   const hydrogenDensity = 0.0899; //at 0C, 101kPa
   const airDensity = 1.205; //at 0C, 101kPa
   var gasDensity = 0.0;
-  if (gasType === 'helium'){gasDensity = heliumDensity;} else if(gasType === 'hydrogen'){gasDensity = hydrogenDensity};
+  if (gasType === 'helium') {gasDensity = heliumDensity;} else if (gasType === 'hydrogen') {gasDensity = hydrogenDensity}
+  ;
   const airDensityModel = 7283.3;
-
-  var launchDiameter = 2*Math.pow(((3*launchVolume)/(4*Math.PI)), 1/3);
-  var area = Math.PI*Math.pow(launchDiameter/2, 2);
-
+  
+  var launchDiameter = 2 * Math.pow(((3 * launchVolume) / (4 * Math.PI)), 1 / 3);
+  var area = Math.PI * Math.pow(launchDiameter / 2, 2);
+  
   const balloonWeightString = balloonWeight.toString();
   var burstDiameter = totexDataDict[balloonWeightString][0];
-  var burstVolume = (4/3)*Math.PI*Math.pow(burstDiameter/2,3);
-  var burstVolumeRatio = burstVolume/launchVolume;
-  var burstHeightMeters = -(airDensityModel*Math.log(1/burstVolumeRatio));
-
-  var grossLift = launchVolume*(airDensity-gasDensity);
-  var freeLift = grossLift-((balloonWeight+payloadWeight)/1000);
-  var balloonCd =totexDataDict[balloonWeightString][1];
-  var ascentRateMeters = Math.sqrt(freeLift/(0.5*balloonCd*airDensity*area));
-  var ascentRateFeet = (ascentRateMeters*3.28)*60; //also convert from m/sec to ft/min
-  var neutralLift = payloadWeight/1000+freeLift;
+  var burstVolume = (4 / 3) * Math.PI * Math.pow(burstDiameter / 2, 3);
+  var burstVolumeRatio = burstVolume / launchVolume;
+  var burstHeightMeters = - (airDensityModel * Math.log(1 / burstVolumeRatio));
+  
+  var grossLift = launchVolume * (airDensity - gasDensity);
+  var freeLift = grossLift - ((balloonWeight + payloadWeight) / 1000);
+  var balloonCd = totexDataDict[balloonWeightString][1];
+  var ascentRateMeters = Math.sqrt(freeLift / (0.5 * balloonCd * airDensity * area));
+  var ascentRateFeet = (ascentRateMeters * 3.28) * 60; //also convert from m/sec to ft/min
+  var neutralLift = payloadWeight / 1000 + freeLift;
   var burstHeightFeet = burstHeightMeters * 3.28;
-  var timeToBurst = burstHeightFeet/ascentRateFeet;
-
+  var timeToBurst = burstHeightFeet / ascentRateFeet;
+  
   return [burstHeightFeet, ascentRateFeet, neutralLift, timeToBurst];
 }
 
 //This handles the post request made by the client
 //TODO: return the global requests list/chain/linkedlist object?
-app.post('/client_message', function(req, res) {
+app.post('/client_message', function (req, res) {
   // Use URL to parse the request and get a URL object from it.
   var msgObject = req.body;
   
@@ -176,7 +204,7 @@ app.post('/client_message', function(req, res) {
 });
 
 // TODO: setup the rocblock server router to the /incoming url
-app.post('/incoming', function(req, res){
+app.post('/incoming', function (req, res) {
   console.log('incoming detected!');
   
   // The data sent from RB is in req.body
@@ -185,7 +213,7 @@ app.post('/incoming', function(req, res){
   console.log(formData.imei);
   
   // We will log the data
-    //TODO: there is something wrong with the longitudes. It is probably here. Hint: no negative values at all for lat and lng. Temp fix: all longitudes are negative
+  // Temp fix: all longitudes are negative -> We thought this was necessary, but turns out RockBlock test data is always in the +,+ range
   logData(formData.imei, formData.momsn, formData.transmit_time, formData.iridium_latitude, (formData.iridium_longitude), formData.iridium_cep, formData.data);
   
   // RockBlock documentation requires us to respond with http status 200
@@ -195,7 +223,7 @@ app.post('/incoming', function(req, res){
   res.end();
 });
 
-app.post('/send_message', jsonParser, function(req, res) {
+app.post('/send_message', jsonParser, function (req, res) {
   //TODO: Check if this is parsing correctly, record sent messages in db
   var msgObject = req.body;
   rock.send(msgObject.imei, msgObject.username, msgObject.password, msgObject.msg);
@@ -205,18 +233,20 @@ app.post('/send_message', jsonParser, function(req, res) {
 });
 
 // This is how we send messages to the RockBlock servers
-var send = function(imei, username, password, data) {
+var send = function (imei, username, password, data) {
   request.post(
     'https://core.rock7.com/rockblock',
-    { json: {
-      imei: imei,
-      username: username,
-      password: password,
-      data: data,
-    } },
+    {
+      json: {
+        imei: imei,
+        username: username,
+        password: password,
+        data: data,
+      }
+    },
     function (error, response, body) {
       //response + body here
-      if (!error && response.statusCode === 200) {
+      if (! error && response.statusCode === 200) {
         console.log(body);
       }
     }
@@ -240,16 +270,16 @@ app.get('/', function (req, res) {
 
 //formidable file upload for flight data analysis
 var dataPath = '';
-app.post('/upload', function (req, res){
+app.post('/upload', function (req, res) {
   var form = new formidable.IncomingForm();
-
-  form.parse(req, function(err, fields, files) {
+  
+  form.parse(req, function (err, fields, files) {
     res.writeHead(200, {'content-type': 'text/plain'});
     res.write('received upload:\n\n');
     dataPath = file.path;
     res.end(util.inspect({fields: fields, files: files}));
   });
-  form.on('file', function(name, file){
+  form.on('file', function (name, file) {
     console.log('Uploaded' + file.name);
   });
 });
